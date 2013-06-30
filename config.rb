dc5575dec78a6dba868f614f2061b19abb16f200
Nelson Francisco <nelsondelega@gmail.com>
2013-06-29 21:14:04 -0300

    relative assets true

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 # Require any additional compass plugins here.
  
 # Set this to the root of your project when deployed:
-http_path = "http://localhost/sprite-generator/"
+http_path = ""
 css_dir = "css"
 sass_dir = "scss"
 images_dir = "images"
@@ -10,7 +10,7 @@ javascripts_dir = "js"
 # output_style = :expanded
  
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
  
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
